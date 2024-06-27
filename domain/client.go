cbd97a660b5959ef4218335c87aa7e9511742132
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-06-27 13:24:02 -0500

    batman

---
+++ domain/client.go
@@ -0,0 +1,18 @@
+package domain
+
+// ClientCapabilities is a struct for the client capabilities
+//
+// Microsoft LSP Docs:
+// https://microsoft.github.io/language-server-protocol/specifications/specification-current/#clientCapabilities
+type ClientCapabilities struct {
+	// TextDocumentSync is the text document sync for the client capabilities.
+	TextDocumentSync int `json:"textDocumentSync"`
+	// HoverProvider is a boolean indicating whether the server provides.
+	HoverProvider bool `json:"hoverProvider"`
+	// DefinitionProvider is a boolean indicating whether the server provides definition capabilities.
+	DefinitionProvider bool `json:"definitionProvider"`
+	// CodeActionProvider is a boolean indicating whether the server provides code actions.
+	CodeActionProvider bool `json:"codeActionProvider"`
+	// CompletionProvider is a map of completion providers.
+	CompletionProvider map[string]any `json:"completionProvider"`
+}
